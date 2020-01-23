5e9b4d4112f8c5200115be7a8a2936c7e44cd947
EL BAHLOUL Abdelali <bahloul97abdel@gmail.com>
2020-01-23 11:02:11 +0100

    deux méthodes de injection de dependences : par instanciation statique et par instanciation dynamique en utilisant un fichier de configuration

--- DAO/DaoIpml.cs
+++ DAO/DaoIpml.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IOC_DI_Principe
+namespace IOC_DI_Principe.DAO
 {
     class DaoIpml : IDao
     {
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -13,11 +14,31 @@ namespace IOC_DI_Principe
     {
         public static void Main(string[] args)
         {
+            // injection par instanciation statique
             //CreateHostBuilder(args).Build().Run();
-            DaoIpml dao = new DaoIpml();
-            MetierImpl metier = new MetierImpl(dao);
+            //DaoIpml dao = new DaoIpml();
+            //MetierImpl metier = new MetierImpl(dao);
+            //ou methode 2 via le ctroc par default
+            //metier.dao = dao;
+            //Console.WriteLine(metier.calcul());
+            //Console.ReadKey();
+
+            //injection par instanciation dynamique
+            string path = "config.txt";
+            string[] data = File.ReadAllLines(path);
+            string daoClassName = data[0];
+            string metierClassName = data[1];
+
+            IDao dao = (IDao)Activator.CreateInstance(Type.GetType(daoClassName));
+
+            IMetier metier = (IMetier)Activator.CreateInstance(Type.GetType(metierClassName), dao); // c'est l'équivalent de [ MetierImpl metier = new MetierImpl(dao); ]
             Console.WriteLine(metier.calcul());
+
+
+            Console.WriteLine(daoClassName);
+            Console.WriteLine(metierClassName);
             Console.ReadKey();
+
         }
 
         //public static IHostBuilder CreateHostBuilder(string[] args) =>
